a7970552aec622fccf0710e5408f661af9efaa47
Ikenna Enyidede <stephencharles02@gmail.com>
2023-09-07 23:07:38 +0100

    Add bash script

---
+++ script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Create User
+username="sage"
+#password="devops-project"
+
+sudo apt update
+sudo adduser --disabled-password --gecos "" $username
+#sudo passwd $password
+usermod -aG sudo $username
+su - $username
+
+
+# Install Docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo usermod -aG docker ${USER}
+su - ${USER}
+
+# Install minikube and kubectl
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo cp minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod +x /usr/local/bin/minikube
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+minikube start --driver=docker
+minikube status
